52524cdfa25aff9aa13d94ead5638d9fbee3e52f
kataevangelista17-commits <kataevangelista17@gmail.com>
2026-03-23 15:19:34 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,30 +1,41 @@
-//API framework
+//API FRAMEWORD
 const express = require('express');
-//cross origin resource sharing
-const cors=require('cors');
-//Environment variables
-require('dotenv').config();
-//Database connection
-const db=require('./config/db');
-//Routes
-const routes=require('./routes');
-//utilization of express
+//CROSS ORIGIN RESOURCE SHARING
+const cors = require('cors');
+//ENVIRONMENT VARIABLES
+require ('dotenv').config();
+//DATABASE CONNECTION
+const db=require('./config/db.js');
+//ROUTES
+const routes = require('./routes/index.js');
+
+
+//UTILIZATION OF EXPRESS
 const app = express();
+
+
+//MOMENT
+const moment = require('moment');
+const logger = (req, res, next) => {
+    console.log(`${req.protocol}://${req.get('host')} ${req.originalUrl} - ${moment().format()}` );
+    next();
+}
+app.use(logger);
+
+
+
+
+
+
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended:true})); //this will allow us to read the URL body tags
-//Use routes
-app.use('/api',routes)
-//Server listening
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
-});
+app.use(express.urlencoded({extended:true})) //this will allow to read the url body tags
 
 
-app.use(express.json());
-//Use routes
-app.use('/api',routes)
-//Server listening
+//use routes
+app.use('/api', routes)
+
+
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
-});
+})
